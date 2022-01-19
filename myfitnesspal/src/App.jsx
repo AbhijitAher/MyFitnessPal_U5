d56745233c1ef6988ca994186a50cf0bdec27b01
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-01-19 11:02:02 +0530

    first commit

---
+++ myfitnesspal/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {HomePage} from "./components/Homepage"
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
