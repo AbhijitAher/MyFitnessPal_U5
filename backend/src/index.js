18c856e3ac6b41ba7539ca63795b0366c40f5f4e
AbhijitAher <seabhi.aher@gmail.com>
2022-01-21 16:49:18 +0530

    initialized oauth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { login, register } = require("./controllers/auth.controller");
 const userController = require("./controllers/user.controller");
+const passport = require("./configs/passport");
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,20 @@ app.use(express.json());
 app.use("/users", userController);
 app.use("/login", login);
 app.post("/register", register);
+
+// ---------------------- Google OAuth --------------------------
+// app.use(passport.authenticate());
+app.get(
+	"/auth/google",
+	passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+	"/auth/google/callback",
+	passport.authenticate("google", {
+		successRedirect: "/auth/google/success",
+		failureRedirect: "/auth/google/failure",
+	})
+);
 
 module.exports = app;
