a3f2515b6aa89ae38268a46e6c8c26a604f8bf20
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-01-22 22:21:28 +0530

    food css

--- myfitnesspal/src/components/Food.jsx
+++ myfitnesspal/src/components/Food.jsx
@@ -56,11 +56,45 @@ console.log("Data:",data);
         onChange={(e) => setText(e.target.value)}
         placeholder="Search for a food, brand, or restaurant"
       />
-      <button onClick={handleSearch}>
-        <i className="fas fa-search"></i>
+      <button id="foodSearchBtn" onClick={handleSearch}>
+        <i className="fas fa-search"></i> 
       </button>
-      
       <img id="noDataImg" src="https://www.myfitnesspal.com/react-static/e95f17aa29d83b7a7588a0f825f7b66f.svg" alt="" />   
+
+      <div id="home_body_5">
+        <div id="home_body_5_sec1">
+          <h2>Recipes & Inspiration</h2>
+          <p>
+            Get nutritionist-approved recipes and motivational workout tips from
+            MyFitnessPal experts.
+          </p>
+        </div>
+        <div id="food_body_1">
+          
+          <a href="https://blog.myfitnesspal.com/1200-calories-looks-like-infographic?utm_source=mfp&utm_medium=app_homepage">
+            <div>
+              <img
+                src="https://www.myfitnesspal.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcalories-look-like.ccfc01e7.png&w=640&q=75"
+                alt=""
+              />
+              <h3 className="linkHead">
+                What the 1,200-Calorie Diet Looks Like
+              </h3>
+              <p className="blog_p">MyFitnessPal Blog</p>
+            </div>
+          </a>
+          <a href="https://blog.myfitnesspal.com/essential-guide-to-movement-activity/">
+            <div>
+              <img
+                src="https://www.myfitnesspal.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgetting-moving.deb62de2.png&w=640&q=75"
+                alt=""
+              />
+              <h3 className="linkHead">Essential Guide to Getting Moving</h3>
+              <p className="blog_p">MyFitnessPal Blog</p>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
