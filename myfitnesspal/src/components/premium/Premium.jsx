5d99ca252f66f32ef13c8dd701e0a5e7dfb532ee
AbhijitAher <seabhi.aher@gmail.com>
2022-01-23 16:10:54 +0530

    uninstalled chakra, added images

--- myfitnesspal/src/components/premium/Premium.jsx
+++ myfitnesspal/src/components/premium/Premium.jsx
@@ -1,5 +1,10 @@
 export const Premium = () => {
-	return <div>
-        
-    </div>;
+	return (
+		<div>
+			<img
+				src="https://github.com/AbhijitAher/MyFitnessPal_U5/blob/main/myfitnesspal/public/Images/premium/Image.jpg"
+				alt=""
+			/>
+		</div>
+	);
 };
